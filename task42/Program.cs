1a4712fb9aa94fab54125c0d9f89cb3804d3a692
Olga <olga-rovnova@yandex.ru>
2022-06-26 08:05:49 +0300

    task42 file

---
+++ task42/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 42: Напишите программу, 
+//которая будет преобразовывать десятичное число в двоичное. 
+// 45 -> 101101 
+// 3  -> 11 
+// 2  -> 10
+
+int InputInt (string mes) 
+{     
+    Console.Write($"Insert {mes}: ");     
+    return Convert.ToInt32(Console.ReadLine()); 
+}  
+int number = InputInt("number");
+
+string result = string.Empty;
+
+while (number>=2)
+{
+    result= Convert.ToString(number%2)+result;
+    number = number/2;
+}
+result = Convert.ToString(number%2)+result;
+Console.WriteLine(result);
