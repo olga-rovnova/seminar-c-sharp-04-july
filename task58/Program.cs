ed1ed83c113b28fa668ded97d5f5a6e334ba2512
Olga <olga-rovnova@yandex.ru>
2022-07-09 23:56:07 +0300

    task58 file

---
+++ task58/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 64: Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1
+
+Console.Write("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = number;
+
+Console.WriteLine(Numbers(number, count));
+
+string Numbers(int number, int count)
+{
+    if (count == 0)
+    {
+        return string.Empty;
+    }
+    else
+    {
+        return number + " " + Numbers(number - 1, --count);
+    }
+}
